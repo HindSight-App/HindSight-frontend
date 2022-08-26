1534ad3a90f8568d76a82e4f63d08981dbe15cb1
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-08-26 15:14:53 -0700

    styling create page

--- src/pages/Create/Create.tsx
+++ src/pages/Create/Create.tsx
@@ -1,137 +1,54 @@
 import {
   IonContent,
-  IonGrid,
-  IonCol,
   IonPage,
-  IonRow,
-  IonInput,
   IonItem,
-  IonButton,
-  IonImg,
+  IonHeader,
+  IonToolbar,
+  IonList,
+  IonTitle,
+  IonLabel,
+  IonCol,
 } from "@ionic/react";
-import { useState } from "react";
-import {
-  SignupSupabase,
-  setupProfile,
-  uploadFile,
-} from "../../services/AuthUtils.js";
 
 import "./Create.css";
 
-type User = {
-  email: string;
-  password: string;
-};
-type Profile = {
-  username: string;
-};
-const Signup: React.FC = () => {
-  const [profile, setProfile] = useState<Profile>({
-    username: "",
-  });
-  const [user, setUser] = useState<User>({
-    email: "",
-    password: "",
-  });
-  const [file, setFile]: any = useState([]);
-
-  async function handleSignup(e: any) {
-    e.preventDefault();
-    await SignupSupabase(user.email, user.password);
-    await uploadFile(file[0].name, file[0]);
-    await setupProfile(
-      profile.username,
-      `${process.env.REACT_APP_AVATAR_PATH}${file[0].name}`
-    );
-  }
-
+function CreatePage() {
   return (
     <IonPage>
-      <IonContent fullscreen>
-        <form onSubmit={handleSignup}>
-          <IonGrid className="grid">
-            <IonRow>
-              <IonCol>
-                <IonImg src="https://cdn.discordapp.com/attachments/1007867692197355550/1012415331794440273/Hindsight.png"></IonImg>
-              </IonCol>
-            </IonRow>
-            <IonRow className="ion-justify-content-center ion-align-items-center ion-text-center">
-              <IonCol>
-                <IonItem>
-                  <input
-                    type={"file"}
-                    onChange={(e) => {
-                      if (e.target.files) {
-                        file.push(e.target.files[0]);
-                      }
-                    }}
-                  ></input>
-                </IonItem>
-              </IonCol>
-            </IonRow>
-            <IonRow className="ion-justify-content-center ion-align-items-center ion-text-center">
-              <IonCol>
-                <IonItem>
-                  <IonInput
-                    type="email"
-                    autocomplete="email"
-                    inputmode="email"
-                    placeholder="Enter Your Email"
-                    onIonChange={(e) =>
-                      setUser({
-                        email: String(e.target.value),
-                        password: user.password,
-                      })
-                    }
-                  ></IonInput>
-                </IonItem>
-              </IonCol>
-            </IonRow>
-            <IonRow className="ion-justify-content-center ion-align-items-center ion-text-center">
-              <IonCol>
-                <IonItem>
-                  <IonInput
-                    type="text"
-                    inputmode="text"
-                    placeholder="Create A Username"
-                    onIonChange={(e) =>
-                      setProfile({
-                        username: String(e.target.value),
-                      })
-                    }
-                  ></IonInput>
-                </IonItem>
-              </IonCol>
-            </IonRow>
-            <IonRow className="ion-justify-content-center ion-align-items-center ion-text-center">
-              <IonCol>
-                <IonItem>
-                  <IonInput
-                    type="password"
-                    autocomplete="new-password"
-                    placeholder="Create A Password"
-                    onIonChange={(e) =>
-                      setUser({
-                        email: user.email,
-                        password: String(e.target.value),
-                      })
-                    }
-                  ></IonInput>
-                </IonItem>
-              </IonCol>
-            </IonRow>
-            <IonRow className="ion-justify-content-center ion-align-items-center ion-text-center">
-              <IonCol>
-                <IonButton expand="full" type="submit">
-                  Signup
-                </IonButton>
-              </IonCol>
-            </IonRow>
-          </IonGrid>
-        </form>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Create Page</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonList>
+          <IonCol>
+            <IonItem
+              fill="solid"
+              routerLink="/create/ideas"
+              className="ideas-page"
+            >
+              <IonLabel
+                className="create-buttons"
+                class="ion-justify-content-center"
+              >
+                Ideas
+              </IonLabel>
+            </IonItem>
+            <IonItem routerLink="/create/dreams" className="dreams-page">
+              <IonLabel className="create-buttons">Dreams</IonLabel>
+            </IonItem>
+            <IonItem routerLink="/create/memories" className="memories-page">
+              <IonLabel className="create-buttons">Memories</IonLabel>
+            </IonItem>
+            <IonItem routerLink="/create/thoughts" className="thoughts-page">
+              <IonLabel className="create-buttons">Thoughts</IonLabel>
+            </IonItem>
+          </IonCol>
+        </IonList>
       </IonContent>
     </IonPage>
   );
-};
+}
 
-export default Signup;
+export default CreatePage;
