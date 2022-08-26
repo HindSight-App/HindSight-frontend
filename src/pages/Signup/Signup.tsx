2b61200ded3bacdd06118f099b8b55cf78be61ae
Kevin Roney <ktroney.pdx@gmail.com>
2022-08-25 17:55:39 -0700

    safety commit, files no upload will bughunt tmrw

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -7,92 +7,108 @@ import {
   IonInput,
   IonItem,
   IonButton,
-  IonText,
   IonImg,
 } from "@ionic/react";
 import { useState } from "react";
-import { Signup, setupProfile } from '../../services/AuthUtils.js'
+import { SignupSupabase, setupProfile, uploadFile } from '../../services/AuthUtils.js'
 
 import "./Signup.css";
 
-
-
+type User = { 
+  email:string;
+  password:string;
+};
+type Profile = {
+  username:string
+}
 const Signup: React.FC = () => {
-  const [profile, setProfile] = useState({
-    username: '',
-    avatar: '',
+  const [profile, setProfile] = useState<Profile>({
+    username: ''
   })
-  const [user, setUser] = useState({
-    email: '',
+  const [user, setUser] = useState<User>({
+    email: '', 
     password: ''
   })
-
-  function handleSignup() {
-    Signup(user.email, user.password);
-    setupProfile(profile.username, profile.password);
+  const [file, setFile] = useState({})
+  console.log('hello');
+  async function handleSignup(e:any) {
+    e.preventDefault()
+    console.log(e.target);
+    // const [file] = e.target.files;
+    await SignupSupabase(user.email, user.password);
+    await setupProfile(profile.username);
+    await uploadFile(file);
   }
+
   return (
     <IonPage>
       <IonContent fullscreen>
-        <IonGrid className="grid">
-          <IonRow>
-            <IonCol>
-              <IonImg src="https://cdn.discordapp.com/attachments/1007867692197355550/1012415331794440273/Hindsight.png"></IonImg>
-            </IonCol>
-          </IonRow>
-          <IonRow className="ion-justify-content-center ion-align-items-center ion-text-center">
-            <IonCol>
-              <IonItem>
-                <input type={'file'}></input>
-              </IonItem>
-            </IonCol>
-          </IonRow>
-          <IonRow className="ion-justify-content-center ion-align-items-center ion-text-center">
-            <IonCol>
-              <IonItem>
-                <IonInput
-                  type="email"
-                  autocomplete="email"
-                  inputmode="email"
-                  placeholder="Enter Your Email"
-                  onIonChange={e => setUser({
-                    email: e.target.value,
-                    password: {...user}
-                  })}
-                ></IonInput>
-              </IonItem>
-            </IonCol>
-          </IonRow>
-          <IonRow className="ion-justify-content-center ion-align-items-center ion-text-center">
-            <IonCol>
-              <IonItem>
-                <IonInput
-                  type="text"
-                  inputmode="text"
-                  placeholder="Create A Username"
-                ></IonInput>
-              </IonItem>
-            </IonCol>
-          </IonRow>
-          <IonRow className="ion-justify-content-center ion-align-items-center ion-text-center">
-            <IonCol>
-              <IonItem>
-                <IonInput
-                  type="password"
-                  autocomplete="new-password"
-                  inputmode="text"
-                  placeholder="Create A Password"
-                  pattern="password"
-                ></IonInput>
-              </IonItem>
-            </IonCol>
-          </IonRow>
-          <IonRow className="ion-justify-content-center ion-align-items-center ion-text-center">
-            <IonCol>
-              <IonButton expand="full" type="submit">Signup</IonButton>
-            </IonCol>
-          </IonRow>
-        </IonGrid>
+        <form onSubmit={handleSignup}>
+          <IonGrid className="grid">
+            <IonRow>
+              <IonCol>
+                <IonImg src="https://cdn.discordapp.com/attachments/1007867692197355550/1012415331794440273/Hindsight.png"></IonImg>
+              </IonCol>
+            </IonRow>
+            <IonRow className="ion-justify-content-center ion-align-items-center ion-text-center">
+              <IonCol>
+                <IonItem>
+                  <input type={'file'} onChange={e => console.log(e.target.files[0])}></input>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+            <IonRow className="ion-justify-content-center ion-align-items-center ion-text-center">
+              <IonCol>
+                <IonItem>
+                  <IonInput
+                    type="email"
+                    autocomplete="email"
+                    inputmode="email"
+                    placeholder="Enter Your Email"
+                    onIonChange={e => setUser({
+                      email: String(e.target.value),
+                      password: user.password
+                    })}
+                  ></IonInput>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+            <IonRow className="ion-justify-content-center ion-align-items-center ion-text-center">
+              <IonCol>
+                <IonItem>
+                  <IonInput
+                    type="text"
+                    inputmode="text"
+                    placeholder="Create A Username"
+                    onIonChange={e => setProfile({
+                      username: String(e.target.value)
+                    })}
+                  ></IonInput>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+            <IonRow className="ion-justify-content-center ion-align-items-center ion-text-center">
+              <IonCol>
+                <IonItem>
+                  <IonInput
+                    type="password"
+                    autocomplete="new-password"
+                    placeholder="Create A Password"
+                    onIonChange={e => setUser({
+                      email: user.email,
+                      password: String(e.target.value)
+                    })}
+                  ></IonInput>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+            <IonRow className="ion-justify-content-center ion-align-items-center ion-text-center">
+              <IonCol>
+                <IonButton expand="full" type="submit">Signup</IonButton>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </form>
       </IonContent>
     </IonPage>
   );
--- src/services/AuthUtils.js
+++ src/services/AuthUtils.js
@@ -1,13 +1,20 @@
 import { client, checkError } from './client.js';
 
-export async function Signup(email, password){
+export async function SignupSupabase(email, password){
   const response = await client.auth.signUp({ email, password });
   return response.user;
 }
 
-export async function setupProfile(username, avatar) {
+export async function setupProfile(username) {
   const response = await client
-    .from('profile')
-    .insert({ username, avatar });
+    .from('Profile')
+    .insert({ username });
+  return checkError(response);
+}
+
+export async function uploadFile(file) {
+  const response = await client.storage
+    .from('avatars')
+    .upload(file);
   return checkError(response);
 }
