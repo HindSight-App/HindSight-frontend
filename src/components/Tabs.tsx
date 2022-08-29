8296f3123b1b67120955158960bcdbd04f2583f5
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-08-29 14:56:02 -0700

    adding icons and styling tabs

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,29 +1,44 @@
-import React from 'react';
-import { IonItem, IonGrid, IonRow, IonCol, IonLabel } from '@ionic/react';
-import "./Tabs.css"
-
-
+import React from "react";
+import {
+  IonItem,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonLabel,
+  IonIcon,
+} from "@ionic/react";
+import { homeSharp, searchSharp, addCircleSharp } from "ionicons/icons";
+import "./Tabs.css";
 
 export const Tabs: React.FC = () => (
-    <IonGrid>
-      <IonRow class='tabs'>
-          <IonCol>
-            <IonItem class='tab-button' routerLink="/dashboard">
-              <IonLabel >Home</IonLabel>
-            </IonItem>
-          </IonCol>
-          <IonCol >
-            <IonItem class='tab-button' routerLink="/search">
+  <IonGrid>
+    <IonRow class="tabs">
+      <IonCol>
+        <IonItem class="tab-button" routerLink="/dashboard">
+          <div className="input-wrapper">
+            <IonIcon class="local-icon" icon={homeSharp} />
+            <IonLabel>Home</IonLabel>
+          </div>
+        </IonItem>
+      </IonCol>
+      <IonCol>
+        <IonItem class="tab-button" routerLink="/search">
+          <div className="input-wrapper">
+            <IonIcon class="local-icon" icon={searchSharp} />
             <IonLabel>Search</IonLabel>
-            </IonItem>
-          </IonCol>
-          <IonCol>
-            <IonItem class='tab-button' routerLink="/create">
+          </div>
+        </IonItem>
+      </IonCol>
+      <IonCol>
+        <IonItem class="tab-button" routerLink="/create">
+          <div className="input-wrapper">
+            <IonIcon class="local-icon" icon={addCircleSharp} />
             <IonLabel>Create</IonLabel>
-            </IonItem>
-          </IonCol>
-      </IonRow>
-    </IonGrid>
+          </div>
+        </IonItem>
+      </IonCol>
+    </IonRow>
+  </IonGrid>
 );
 
 export default Tabs;
