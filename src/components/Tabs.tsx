5b4bcb39949ab6537e4cc9073e5f81ccee0321f6
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-08-31 11:09:06 -0700

    styled bottom tabs bar

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,18 +1,6 @@
 import React from "react";
-import {
-  IonItem,
-  IonGrid,
-  IonRow,
-  IonCol,
-  IonLabel,
-  IonIcon,
-} from "@ionic/react";
-import {
-  homeSharp,
-  searchSharp,
-  addCircleSharp,
-  addCircle,
-} from "ionicons/icons";
+import { IonGrid, IonRow, IonCol, IonLabel, IonIcon } from "@ionic/react";
+import { homeSharp, addCircleSharp } from "ionicons/icons";
 import "./Tabs.css";
 import { useHistory } from "react-router";
 
@@ -42,28 +30,5 @@ function Tabs() {
     </IonGrid>
   );
 }
-
-// export const Tabs: React.FC = () => (
-//   <IonGrid>
-//     <IonRow class="tabs">
-//       <IonCol>
-//         <div onClick={"/dashboard"}>
-//           <div className="input-wrapper">
-//             <IonIcon class="local-icon" icon={homeSharp} />
-//             <IonLabel>Home</IonLabel>
-//           </div>
-//         </div>
-//       </IonCol>
-//       <IonCol>
-//         <IonItem class="tab-button" routerLink="/create">
-//           <div className="input-wrapper">
-//             <IonIcon class="local-icon" icon={addCircleSharp} />
-//             <IonLabel>Create</IonLabel>
-//           </div>
-//         </IonItem>
-//       </IonCol>
-//     </IonRow>
-//   </IonGrid>
-// );
 
 export default Tabs;
